425faac84a689c798d3d2bfd1989398fbe624a8c
Tanisha Patidar <tanishapatidar905@gmail.com>
2024-11-12 15:28:14 +0530

    sdf

--- src/apiService.js
+++ src/apiService.js
@@ -5,14 +5,16 @@ export const BASE_URL = 'https://dev.talspo.com/admin/api';
 export const SIGNUP_URL = `${BASE_URL}/register`;
 export const LOGIN_URL = `${BASE_URL}/login-view`;
 export const MESSAGE_URL = `${BASE_URL}/massage-create`;
+
 export const BLOG_POSTS_URL = `${BASE_URL}/recent-post-view`;
 export const FEATURED_BLOG_URL = `${BASE_URL}/featured-blogs-view`;
-export const JOB_POST_URL = `${BASE_URL}/job-post-view`;
-export const ABOUT_US_URL = `${BASE_URL}/about-us-view`;
 export const BLOG_VIEW_URL = `${BASE_URL}/blog-view`;
 
+export const JOB_POST_URL = `${BASE_URL}/job-post-view`;
+export const ABOUT_US_URL = `${BASE_URL}/about-us-view`;
 export const TAL_SPO_SKILLED_VIEW_URL = `${BASE_URL}/talspo-skilled-view`;
 
+// ---------------------job opportunities-----------------------------
 
 export const fetchJobPosts = async () => {
   try {
@@ -25,10 +27,12 @@ export const fetchJobPosts = async () => {
   }
 };
 
+// ---------------------About us------------------------------------
+
 export const fetchAboutUs = async () => {
   try {
     const response = await axios.get(ABOUT_US_URL);
-    console.log("Fetched About Us Data:", response.data);
+    // console.log("Fetched About Us Data:", response.data);
     return response.data || {};
   } catch (error) {
     console.error('Error fetching About Us data:', error);
@@ -36,10 +40,12 @@ export const fetchAboutUs = async () => {
   }
 };
 
+// ---------------------skilled candidates-------------------------
+
 export const fetchTalspoSkilledView = async () => {
   try {
     const response = await axios.get(TAL_SPO_SKILLED_VIEW_URL);
-    console.log("Fetched Talspo Skilled Data:", response.data);
+    console.log("Skilled Candidates Response:", response.data);
     return response.data.records || [];
   } catch (error) {
     console.error('Error fetching Talspo Skilled data:', error);
@@ -47,10 +53,12 @@ export const fetchTalspoSkilledView = async () => {
   }
 };
 
+// ------------------Blogs Apis----------------------------------
+
 export const recentBlogPosts = async () => {
   try {
     const response = await axios.get(BLOG_POSTS_URL);
-    console.log(" Recent Blogs:", response.data);
+    // console.log(" Recent Blogs:", response.data);
     return response.data.records || [];
   } catch (error) {
     console.error('Error fetching recent blog posts:', error);
@@ -61,7 +69,7 @@ export const recentBlogPosts = async () => {
 export const featuredBlogPosts = async () => {
   try {
     const response = await axios.get(FEATURED_BLOG_URL);
-    console.log("Featured Blogs:", response.data);
+    // console.log("Featured Blogs:", response.data);
     return response.data.records || [];
   } catch (error) {
     console.error('Error fetching featured blog posts:', error);
@@ -72,15 +80,15 @@ export const featuredBlogPosts = async () => {
 export const fetchBlogDetail = async (id) => {
   try {
     const response = await axios.get(`${BASE_URL}/featured-blog-single-record/${id}`);
-    console.log("API Response:", response.data);
-    
-    return response.data.records || null; // Adjust if necessary
+    console.log("Blog detail view:", response.data);
+    return response.data.records || null; 
   } catch (error) {
     console.error("Error fetching blog detail:", error);
-    return null; // Return null on error
+    return null; 
   }
 };
 
+// ---------------------------------------------------------------------------------
 
 
 
